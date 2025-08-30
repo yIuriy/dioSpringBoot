bcab705c7e097bdf36672450fffe09fb8453a1ca
yIuriy <contatoiuri20@gmail.com>
2025-08-30 12:04:32 -0300

    created mappers

--- storefront/src/main/java/br/com/iuri/storefront/mapper/IProductMapper.java
+++ storefront/src/main/java/br/com/iuri/storefront/mapper/IProductMapper.java
@@ -1,10 +1,17 @@
 package br.com.iuri.storefront.mapper;
 
+import br.com.iuri.storefront.controller.request.ProductSaveRequest;
+import br.com.iuri.storefront.controller.response.ProductAvailableResponse;
+import br.com.iuri.storefront.controller.response.ProductDetailResponse;
+import br.com.iuri.storefront.controller.response.ProductSavedResponse;
+import br.com.iuri.storefront.dto.ProductDetailDTO;
 import br.com.iuri.storefront.dto.ProductInfoDTO;
 import br.com.iuri.storefront.entity.ProductEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
@@ -12,5 +19,12 @@ import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 public interface IProductMapper {
     ProductInfoDTO toDTO(final ProductEntity entity, final BigDecimal price);
 
+    @Mapping(target = "active", constant = "false")
+    ProductEntity toEntity(final ProductSaveRequest request);
 
+    ProductSavedResponse toResponse(final ProductEntity entity);
+
+    List<ProductAvailableResponse> toResponse(final List<ProductEntity> entities);
+
+    ProductDetailResponse toResponse(final ProductInfoDTO dto);
 }
